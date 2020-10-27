6edc122261eab09b110b21e878bee4c3cd0041b8
tai vuong duy <taivuongduy2@gmail.com>
2020-10-27 12:59:19 +0700

    clone

---
+++ resources/datafiles/datacategory.sql
@@ -0,0 +1,7 @@
+insert into `category` (`Name`,`Image`) values
+('Cà Phê',''),
+('Trà Tươi',''),
+('Trà Sữa',''),
+('Đá Xay',''),
+('Bánh Ngọt',''),
+('Bánh Mặn','');
---
+++ resources/datafiles/datatopping.sql
@@ -0,0 +1,14 @@
+insert into `topping` (`Name`,`Price`,`Status`) values
+('Trân Châu Đen',5000,'Còn hàng'),
+('Trân Châu Trắng',5000,'Còn hàng'),
+('Trân Châu Hoàng Kim',5000,'Còn hàng'),
+('Thạch Rau Câu Vị Cà Phê',8000,'Còn hàng'),
+('Thạch Rau Câu Vị Dâu',8000,'Còn hàng'),
+('Thạch Rau Câu Vị Matcha',8000,'Còn hàng'),
+('Hạt Thủy Tinh Nguyên Vị',8000,'Còn hàng'),
+('Hạt Thủy Tinh Vị Dâu',8000,'Còn hàng'),
+('Hạt Thủy Tinh Vị Cam',8000,'Còn hàng'),
+('Thạch Pudding Trứng',10000,'Còn hàng'),
+('Thạch Pudding Phô Mai',10000,'Còn hàng'),
+('Thạch Pudding Trái Cây',10000,'Còn hàng'),
+('Bánh Flan',15000,'Còn hàng');
